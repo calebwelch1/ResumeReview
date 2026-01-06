daf08c78be320d6bf9cb95b7585be4359d58dd6e
calebwelch1 <calebjohn@posteo.net>
2026-01-06 11:53:48 -0500

    fix styling imports

--- ResumeReview/ResumeReview/obj/Debug/net8.0/ResumeReview.AssemblyInfo.cs
+++ ResumeReview/ResumeReview/obj/Debug/net8.0/ResumeReview.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeReview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567f2a71a3ea53dcbb3c0e1109d847aff936585c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f232572c5a5b986f37927b7858bb5e201cb19f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeReview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeReview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
